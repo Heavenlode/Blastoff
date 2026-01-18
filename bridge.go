ac287c7cf44aa54039bf3d5cf1b111a7b8745640
Wyatt Arent <wyatt@koru.technology>
2026-01-18 19:21:08 +0100

    Fix race condition

--- bridge.go
+++ bridge.go
@@ -7,6 +7,24 @@ import (
 	"github.com/google/uuid"
 )
 
+// Command types for the bridge
+type bridgeCommand int
+
+const (
+	cmdSendPacket bridgeCommand = iota
+	cmdInitialConnect
+	cmdRedirect
+	cmdClose
+)
+
+type bridgeCmd struct {
+	cmd       bridgeCommand
+	packet    *enet.Packet
+	channelID uint8
+	data      []byte // For initial token
+	uuid      uuid.UUID
+}
+
 func (server *BlastoffServer) bridgePeerToRemote(peer *enet.Peer, peerIncomingPacket <-chan bridgePacket, closeSignal <-chan bool) {
 	remoteHost, err := enet.NewHost(nil, 100, 0, 0, 0)
 	var remotePeer *enet.Peer
@@ -19,121 +37,157 @@ func (server *BlastoffServer) bridgePeerToRemote(peer *enet.Peer, peerIncomingPa
 	var peerValidated = false
 	var peerToken []byte
 
-	var redirectChan = make(chan uuid.UUID, 1)
+	// Channel for commands to the main loop (which handles all ENet operations)
+	cmdChan := make(chan bridgeCmd, 100)
 
+	// Goroutine to receive packets from the client and convert to commands
 	go func() {
-		// Handle messages to and from the client
-	peerLoop:
 		for {
 			select {
-			case uuid := <-redirectChan:
-				// The server has indicated that the client should be redirected to another server
-				remotePeer.Disconnect(0)
-				remotePeer, err = remoteHost.Connect(server.remoteAddressMap[uuid], 0, 0)
-				if err != nil {
-					log.Printf("Couldn't connect to server: %s\n", err.Error())
-					break peerLoop
-				}
-				err = remotePeer.SendBytes(peerToken, RemoteAdminChannelId, enet.PacketFlagReliable)
-				if err != nil {
-					log.Printf("Couldn't send token to server: %s\n", err.Error())
-					break peerLoop
-				}
-				remoteHost.Flush()
 			case peerPacket := <-peerIncomingPacket:
 				if peerValidated {
-					// Create a new packet with the same data and flags to avoid ownership issues
-					data := peerPacket.packet.GetData()
-					flags := peerPacket.packet.GetFlags()
-					peerPacket.packet.Destroy()
-					if err := remotePeer.SendBytes(data, peerPacket.channelID, flags); err != nil {
-						log.Printf("Couldn't send packet to server: %s\n", err.Error())
+					cmdChan <- bridgeCmd{
+						cmd:       cmdSendPacket,
+						packet:    peerPacket.packet,
+						channelID: peerPacket.channelID,
 					}
-					remoteHost.Flush()
-					break
-				}
-				// Initialize the connection with the remote host.
-				// First, the client sends a packet:
-				// The first part is the UUID of the remote host they wish to connect to
-				// The second part is the token that the remote host will use to verify the connection
-				var data = peerPacket.packet.GetData()
-				peerPacket.packet.Destroy()
-				peerToken = data
-				// Now we connect to the remote
-				remotePeer, err = remoteHost.Connect(defaultRemote, 250, 0)
-				if err != nil {
-					log.Printf("Couldn't connect to server: %s\n", err.Error())
-					break peerLoop
+				} else {
+					// Initial connection packet with token - send to main loop
+					data := peerPacket.packet.GetData()
+					peerPacket.packet.Destroy()
+					cmdChan <- bridgeCmd{
+						cmd:  cmdInitialConnect,
+						data: data,
+					}
 				}
 			case <-closeSignal:
-				break peerLoop
+				cmdChan <- bridgeCmd{cmd: cmdClose}
+				return
 			}
 		}
-		closed = true
 	}()
 
-	// Handle incoming messages from the remote
-	// These are messages which the remote sends to the client
-	// Unless coming from channel RemoteAdminChannelId, then it's a message for the Blastoff server
+	// Main loop - ALL ENet operations happen here (single goroutine)
 remoteLoop:
 	for {
 		if closed {
 			log.Println("[Bridge] Closed, exiting remote loop")
 			break
 		}
+
+		// Check for commands (non-blocking)
+		select {
+		case cmd := <-cmdChan:
+			switch cmd.cmd {
+			case cmdInitialConnect:
+				peerToken = cmd.data
+				remotePeer, err = remoteHost.Connect(defaultRemote, 250, 0)
+				if err != nil {
+					log.Printf("Couldn't connect to server: %s\n", err.Error())
+					break remoteLoop
+				}
+			case cmdSendPacket:
+				if remotePeer != nil && peerValidated {
+					data := cmd.packet.GetData()
+					flags := cmd.packet.GetFlags()
+					cmd.packet.Destroy()
+					if err := remotePeer.SendBytes(data, cmd.channelID, flags); err != nil {
+						log.Printf("Couldn't send packet to server: %s\n", err.Error())
+					}
+					remoteHost.Flush()
+				} else {
+					cmd.packet.Destroy()
+				}
+			case cmdRedirect:
+				if remotePeer != nil {
+					remotePeer.Disconnect(0)
+				}
+				remotePeer, err = remoteHost.Connect(server.remoteAddressMap[cmd.uuid], 0, 0)
+				if err != nil {
+					log.Printf("Couldn't connect to server: %s\n", err.Error())
+					break remoteLoop
+				}
+				err = remotePeer.SendBytes(peerToken, RemoteAdminChannelId, enet.PacketFlagReliable)
+				if err != nil {
+					log.Printf("Couldn't send token to server: %s\n", err.Error())
+					break remoteLoop
+				}
+				remoteHost.Flush()
+			case cmdClose:
+				closed = true
+				break remoteLoop
+			}
+		default:
+			// No command, continue to service
+		}
+
+		if remoteHost == nil {
+			log.Println("[Bridge] Remote host is nil, exiting")
+			break
+		}
+
 		ev := remoteHost.Service(10)
 		if ev.GetType() == enet.EventNone {
 			continue
 		}
+
 		switch ev.GetType() {
 		case enet.EventConnect:
+			if remotePeer == nil {
+				log.Println("[Bridge] Remote peer is nil on connect event")
+				break remoteLoop
+			}
 			err = remotePeer.SendBytes(peerToken, RemoteAdminChannelId, enet.PacketFlagReliable)
 			if err != nil {
 				log.Printf("Couldn't send token to server: %s\n", err.Error())
 				break remoteLoop
 			}
 			remoteHost.Flush()
-		case enet.EventDisconnect:
+
+		case enet.EventDisconnect, enet.EventDisconnectTimeout:
+			log.Println("[Bridge] Remote disconnected or timed out")
 			break remoteLoop
 
 		case enet.EventReceive:
 			packet := ev.GetPacket()
-			// log.Printf("[Bridge] Received from remote: channel=%d, size=%d bytes\n", ev.GetChannelID(), packet.GetLength())
 			if ev.GetChannelID() == RemoteAdminChannelId {
 				var data = packet.GetData()
 				packet.Destroy()
-				// This is a special communication packet from the server
-				// The first byte indicates the type of packet
 				switch ServerCommandFlag(data[0]) {
 				case ServerCommandNewInstance:
-					// The server is redirecting the client to another server
-					// The data contains the UUID of the server to redirect to
 					if len(data) < 37 {
 						log.Println("Invalid new instance data")
 						break remoteLoop
 					}
-					uuid, err := uuid.FromBytes(data[1:37])
-					if err != nil {
-						log.Printf("Couldn't parse UUID: %s\n", err.Error())
+					newUUID, parseErr := uuid.FromBytes(data[1:37])
+					if parseErr != nil {
+						log.Printf("Couldn't parse UUID: %s\n", parseErr.Error())
 						break remoteLoop
 					}
-					log.Printf("Spawning new instance: %s\n", uuid.String())
+					log.Printf("Spawning new instance: %s\n", newUUID.String())
 				case ServerCommandValidateClient:
 					peerValidated = true
 				case ServerCommandRedirectClient:
-					// The client is being redirected to another server
-					// The data contains the UUID of the server to redirect to
 					if len(data) < 37 {
 						log.Println("Invalid redirect data length")
 						break remoteLoop
 					}
-					uuid, err := uuid.FromBytes(data[1:37])
-					if err != nil {
-						log.Printf("Couldn't parse UUID: %s\n", err.Error())
+					redirectUUID, parseErr := uuid.FromBytes(data[1:37])
+					if parseErr != nil {
+						log.Printf("Couldn't parse UUID: %s\n", parseErr.Error())
 						break remoteLoop
 					}
-					if _, ok := server.remoteAddressMap[uuid]; !ok {
-						log.Printf("Remote host ID %s not found during redirect.", uuid.String())
+					if _, ok := server.remoteAddressMap[redirectUUID]; !ok {
+						log.Printf("Remote host ID %s not found during redirect.", redirectUUID.String())
+						break remoteLoop
+					}
+					// Handle redirect in this goroutine (safe - all ENet ops in main loop)
+					if remotePeer != nil {
+						remotePeer.Disconnect(0)
+					}
+					remotePeer, err = remoteHost.Connect(server.remoteAddressMap[redirectUUID], 0, 0)
+					if err != nil {
+						log.Printf("Couldn't connect to server: %s\n", err.Error())
 						break remoteLoop
 					}
 				default:
@@ -143,14 +197,11 @@ remoteLoop:
 				continue
 			} else {
 				if !peerValidated {
-					// This is a bug. The server should not be sending messages through this stream before the client is validated.
 					log.Println("Client not validated.")
 					break remoteLoop
 				}
-				// Create a new packet with the same data and flags to avoid ownership issues
 				data := packet.GetData()
 				flags := packet.GetFlags()
-				// log.Printf("[Bridge] Forwarding to client: channel=%d, size=%d bytes, flags=%d\n", ev.GetChannelID(), len(data), flags)
 				packet.Destroy()
 				if err := peer.SendBytes(data, ev.GetChannelID(), flags); err != nil {
 					log.Printf("Couldn't send packet to client: %s\n", err.Error())
@@ -158,6 +209,12 @@ remoteLoop:
 			}
 		}
 	}
-	peer.Disconnect(0)
-	remoteHost.Destroy()
+
+	if peer != nil {
+		peer.Disconnect(0)
+	}
+	if remoteHost != nil {
+		remoteHost.Destroy()
+	}
+	log.Println("[Bridge] Bridge closed")
 }
